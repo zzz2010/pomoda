184c84260e9c75be86ab82bd204ed5419e0e9cb4
zzz2010@gmail.com <zzz2010@gmail.com@03989808-e61b-11de-b362-3513cf04e977>
2011-08-16 13:33:14 +0000

    result good, fix it

--- Pomoda/src/SamplingThread_PS.java
+++ Pomoda/src/SamplingThread_PS.java
@@ -16,7 +16,7 @@ public class SamplingThread_PS extends Thread {
 	PWM motif;
 	int samplenum;
 	String pattern;
-	Random rand=new Random(0);
+	Random rand=new Random(123456789);
 	int startSeqId;
 	int mismatch;
 	int dbsize;
