961d5d0c9db818ca907328fb707a00fc63375cd2
zzz2010@gmail.com <zzz2010@gmail.com@03989808-e61b-11de-b362-3513cf04e977>
2011-09-30 05:14:55 +0000

    fix WMP bug using "N" in DProbMap to get the dmer length, add spearman correlation in PBM

--- Pomoda/src/common.java
+++ Pomoda/src/common.java
@@ -10,6 +10,8 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.biojava.bio.dist.Distribution;
 import org.biojava.bio.dist.DistributionFactory;
@@ -1249,6 +1251,18 @@ public class common {
 	    return maxLen;
 	}
 	
+	public 	static <K,V extends Comparable<? super V>> SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
+        SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
+                new Comparator<Map.Entry<K,V>>() {
+                    @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
+                        int res = e1.getValue().compareTo(e2.getValue());
+                        return res != 0 ? res : 1; // Special fix to preserve items with equal values
+                    }
+                }
+            );
+            sortedEntries.addAll(map.entrySet());
+            return sortedEntries;
+        }
 	
 	public static double getPearsonCorrelation(double[] scores1,double[] scores2){
         double result = 0;
